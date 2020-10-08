1db0d9cb75fbd6cf8409653f7669b2f31698de32
axk4647 <arturokuang@gmail.com>
2020-10-07 21:37:05 -0400

    recyclerview now uses images

--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -1,12 +1,14 @@
 package com.example.photogallery
 
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewTreeObserver
-import android.widget.TextView
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -50,22 +52,29 @@ class PhotoGalleryFragment : Fragment() {
             })
     }
 
-    private class PhotoHolder(itemTextView: TextView) : RecyclerView.ViewHolder(itemTextView) {
+    private class PhotoHolder(private val itemImageView: ImageView) : RecyclerView.ViewHolder(itemImageView) {
 
-        val bindTitle: (CharSequence) -> Unit = itemTextView::setText
+        val bindDrawable: (Drawable) -> Unit = itemImageView::setImageDrawable
     }
 
-    private class PhotoAdapter(private val galleryItems: List<GalleryItem>) :
+    private inner class PhotoAdapter(private val galleryItems: List<GalleryItem>) :
         RecyclerView.Adapter<PhotoHolder>() {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
-            val textView = TextView(parent.context)
-            return PhotoHolder(textView)
+            val view = layoutInflater.inflate(
+                R.layout.list_item_gallery,
+                parent,
+                false
+            ) as ImageView
+            return PhotoHolder(view)
         }
 
         override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
             val galleryItem = galleryItems[position]
-            holder.bindTitle(galleryItem.title)
+            val placeHolder: Drawable = ContextCompat.getDrawable(
+                requireContext(),
+                R.drawable.bill_up_close) ?: ColorDrawable()
+            holder.bindDrawable(placeHolder)
         }
 
         override fun getItemCount(): Int = galleryItems.size
