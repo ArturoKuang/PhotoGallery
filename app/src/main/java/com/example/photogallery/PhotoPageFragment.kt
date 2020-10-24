7d6b88300f8ea2c851c4edcc1e32b47817b0b19e
axk4647 <arturokuang@gmail.com>
2020-10-23 23:36:33 -0400

    webview can now navigate backwards

--- app/src/main/java/com/example/photogallery/PhotoPageFragment.kt
+++ app/src/main/java/com/example/photogallery/PhotoPageFragment.kt
@@ -2,21 +2,24 @@ package com.example.photogallery
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
 
 private const val ARG_URI = "photo_page_url"
-class PhotoPageFragment : VisibleFragment() {
+private const val TAG = "PhotoPageFragment"
+
+class PhotoPageFragment() : VisibleFragment() {
 
     private lateinit var uri: Uri
     private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         uri = arguments?.getParcelable(ARG_URI) ?: Uri.EMPTY
     }
 
@@ -31,9 +34,21 @@ class PhotoPageFragment : VisibleFragment() {
         webView.settings.javaScriptEnabled = true
         webView.webViewClient = WebViewClient()
         webView.loadUrl(uri.toString())
-        
+        val callback = requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner, object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if(webView.canGoBack()) {
+                        webView.goBack()
+                    } else {
+                        isEnabled = false
+                        requireActivity().onBackPressed()
+                    }
+                }
+            })
+
         return view
     }
+
 
     companion object {
         fun newInstance(uri: Uri): PhotoPageFragment {
